74fc5e0ad058b4444794f9884414277011b076d5
wenzzzel <wenzzzel@live.se>
2021-09-25 21:09:22 +0200

    Created Excercise model

---
+++ Models/Excercise.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace excercise_api
+{
+    public class Excercice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
