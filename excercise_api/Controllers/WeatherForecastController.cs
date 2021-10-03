4719b7ed4493c5e663995f4dd35b52c291f356c0
wenzzzel <wenzzzel@live.se>
2021-10-03 12:55:34 +0200

    Removed logger part

--- excercise_api/Controllers/WeatherForecastController.cs
+++ excercise_api/Controllers/WeatherForecastController.cs
@@ -17,12 +17,12 @@ namespace excercise_api.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        // private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
