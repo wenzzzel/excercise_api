d87509a82c9717e1f890fb37a81c49a3a453e33a
wenzzzel <wenzzzel@live.se>
2021-10-03 13:02:15 +0200

    Added example test

--- excercise_api.UnitTests/UnitTest1.cs
+++ excercise_api.UnitTests/UnitTest1.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
+using excercise_api.Controllers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace excercise_api.UnitTests
 {
-    public class Tests
+    public class WeatherFOrecastControllerTests
     {
         [SetUp]
         public void Setup()
@@ -10,9 +13,19 @@ namespace excercise_api.UnitTests
         }
 
         [Test]
-        public void Test1()
+        public void Get_WhenCalled_WeatherSummaryIsHardcoded()
         {
-            Assert.Pass();
+            //Arrange
+            WeatherForecastController myWeatherForecastController = new WeatherForecastController();
+
+            //Act
+            IEnumerable<WeatherForecast> myWeatherForecast = myWeatherForecastController.Get();
+            string[] validWeathers = new string[] {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            //Assert
+            Assert.Contains(myWeatherForecast.First().Summary, validWeathers);
         }
     }
 }
