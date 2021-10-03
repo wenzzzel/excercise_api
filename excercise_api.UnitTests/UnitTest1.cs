ad1515317684dab9ae562967719d12a7e796403b
wenzzzel <wenzzzel@live.se>
2021-10-03 12:28:49 +0200

    Added scaffold for nunit sollution

---
+++ excercise_api.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace excercise_api.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
