176acc5ebb4004e939f1fb58a391c6430d568aff
wenzzzel <wenzzzel@live.se>
2021-10-10 13:50:45 +0200

    Made DateCreated required

--- excercise_api/Models/Excercise.cs
+++ excercise_api/Models/Excercise.cs
@@ -8,6 +8,7 @@ namespace excercise_api
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
     }
 }
